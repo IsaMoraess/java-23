68a5031c287ab32281ea0e593ce65e2b59faf3a0
IsaMoraess <135661414+IsaMoraess@users.noreply.github.com>
2024-03-31 10:26:45 -0300

    Add files via upload

---
+++ LetraGrande.java
@@ -0,0 +1,11 @@
+public class LetraGrande {
+    public static void main(String[] args) {
+        // Definindo a letra a ser exibida
+                System.out.println("L");
+                System.out.println("L");
+                System.out.println("L");
+                System.out.println("L");
+                System.out.println("LLLLL");
+        }
+    }
+
